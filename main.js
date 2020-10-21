4e83f711d420c541daad06797503d7f2c38c3a84
demetrio <demeuforia@gmail.com>
2020-10-21 21:28:04 +0200

    hasta aqui he llegaooo

---
+++ main.js
@@ -0,0 +1,10 @@
+// creo q tengo q guardar la variable de la seleccion del elemento q quiero hacer cambios 
+let caja1 = document.querySelector("#idcaja");
+// pienso q click es un evento y palabra reservada pero quizas no va donde la pongo, no se seguir en este paso
+funcion click(nto){
+// aqui vendrian los cambios como la clase verde
+caja1.getElementsByClassName("clasecaja").verde;
+}
+// aqui llamo a esperar escucha del elemento caja1 al hacer click, pero ahora dudo de q ese click haga algo al estar en modo string, lo recuerdo
+//asi pero lo dudo ahora
+document.addEventListener("click",caja1);
