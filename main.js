b76068c232ed426b4a6640053c18882b9f81f2c0
demetrio <demeuforia@gmail.com>
2020-10-23 00:27:27 +0200

    yeah!

--- main.js
+++ main.js
@@ -1,9 +1,37 @@
 document.getElementById("pulsar").addEventListener("click", escuchoclickboton); 
-function escuchoclickboton() { document.querySelector("#uno").className = "verde"; }
-​
 
-/*
-    if (  input.value === "" ) {
-        document.querySelector("#idcaja").className = "rojo";
-        alert("ERROR , el campo esta vacio")
-*/
+function escuchoclickboton() { 
+
+    let caja1 = document.getElementById("uno");
+
+        let caja2 = document.querySelector("#dos");
+
+            let caja3 = document.querySelector("#tres");
+    
+                    if ( caja1.className === "negro" && caja2.className === "negro" && caja3.className === "negro" ){
+                        document.querySelector("#uno").className = "verde"; 
+
+                    }
+
+                            else if( caja2.className === "negro" && caja3.className === "negro" ){
+
+                                document.querySelector("#uno").className = "negro";
+
+                                document.querySelector("#dos").className = "ambar"; 
+                            }
+
+                                    else if( caja1.className === "negro" && caja3.className === "negro" ){
+
+                                        document.querySelector("#dos").className = "negro"; 
+                                        
+                                        document.querySelector("#tres").className = "rojo"; 
+                                    
+                                    }
+
+                                            else {
+
+                                                document.querySelector("#tres").className = "negro"; 
+                                            }
+
+}
+
