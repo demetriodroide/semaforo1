59a38d070cf6343ba550fd1d3d842b444d610627
unknown <[EMAIL]>
2020-10-24 18:01:30 +0200

    incapz

--- main.js
+++ main.js
@@ -1,37 +1,53 @@
 document.getElementById("pulsar").addEventListener("click", escuchoclickboton); 
 
 function escuchoclickboton() { 
+   // let cuartacaja = document.querySelector("#cuarta");
+let todas = document.querySelector(".negro");
+        if ( todas.className === "negro" ) {
+        //  document.querySelector("#cuarta").className = "verde"; 
+        document.querySelector("#cuarta").className = "verde"; 
+        }
+        let todoslosdiv = document.querySelectorAll("div");
+        console.log(todoslosdiv.length);
 
+
+
+
+
+
+
+
+
+
+
+
+        /*
+                else if (  ){
+
+                }
+
+
+                */
+} /*
     let caja1 = document.getElementById("uno");
-
-        let caja2 = document.querySelector("#dos");
-
-            let caja3 = document.querySelector("#tres");
+    let caja2 = document.querySelector("#dos");
+    let caja3 = document.querySelector("#tres");
     
-                    if ( caja1.className === "negro" && caja2.className === "negro" && caja3.className === "negro" ){
-                        document.querySelector("#uno").className = "verde"; 
+                if ( caja1.className === "negro" && caja2.className === "negro" && caja3.className === "negro" ){
+                    document.querySelector("#uno").className = "verde"; 
+                  }
 
-                    }
-
-                            else if( caja2.className === "negro" && caja3.className === "negro" ){
-
-                                document.querySelector("#uno").className = "negro";
-
-                                document.querySelector("#dos").className = "ambar"; 
+                 else if( caja2.className === "negro" && caja3.className === "negro" ){
+                  document.querySelector("#uno").className = "negro";
+                   document.querySelector("#dos").className = "ambar"; 
                             }
 
-                                    else if( caja1.className === "negro" && caja3.className === "negro" ){
-
-                                        document.querySelector("#dos").className = "negro"; 
-                                        
-                                        document.querySelector("#tres").className = "rojo"; 
-                                    
+                  else if( caja1.className === "negro" && caja3.className === "negro" ){
+                    document.querySelector("#dos").className = "negro"; 
+                   document.querySelector("#tres").className = "rojo"; 
                                     }
-
-                                            else {
-
-                                                document.querySelector("#tres").className = "negro"; 
-                                            }
-
-}
+                      else {
+                      document.querySelector("#tres").className = "negro"; 
+                           }
+*/
 
