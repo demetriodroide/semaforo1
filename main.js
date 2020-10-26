67267249fc70d4888f8425c695a29a1f2d4562e8
unknown <[EMAIL]>
2020-10-26 17:16:55 +0100

    semaforo completo infinito

--- main.js
+++ main.js
@@ -1,15 +1,65 @@
 document.getElementById("pulsar").addEventListener("click", escuchoclickboton); 
 
 function escuchoclickboton() { 
-   // let cuartacaja = document.querySelector("#cuarta");
-let todas = document.querySelector(".negro");
-        if ( todas.className === "negro" ) {
-        //  document.querySelector("#cuarta").className = "verde"; 
-        document.querySelector("#cuarta").className = "verde"; 
+   
+let todaslascajas = document.querySelectorAll(".todas");
+let cajaverde = document.querySelector(".verde.todas");
+        console.log(cajaverde);
+        //null == false
+        //Elemento == true
+        if (cajaverde) {
+          //Ponerla en negro y la siguiente en verde
+
+          /*
+            i = 2;
+            Negro //
+            Negro //
+            ->Verde //Termina el for sin entrar dentro
+            Negro
+
+          */
+         console.log("Entra");
+          let i = 0;
+          while (todaslascajas[i].className === "negro todas")
+          {
+            i++;
+          }
+          // for (i = 0; todaslascajas[i].className === "negro todas"; i++ ){}
+          console.log(i);
+          todaslascajas[i].className = "negro todas";
+              // if ( todaslascajas[i + 1] === undefined ) {
+              //   i = -1;
+              // }
+              // todaslascajas[++i].className = "verde todas";
+              if ( todaslascajas[i + 1] !== undefined ) {
+                todaslascajas[++i].className = "verde todas";
+              }
+            }
+        else {
+          //Pintar la primera de verde
+          todaslascajas[0].className = "verde todas";
         }
+
+
+
+
+        // if ( todaslascajas.className === "verde" ) {
+         
+        // for (let i = 0; i < todaslascajas.className === "verde"; i++ ){
+        //   todaslascajas[i].className === "todas negro";
+        //   todaslascajas[i++].className === "todas verde";
+            
+        // }
+
+        // }
+       
+        // else {
+        //   todaslascajas[0].className = "todas negro"
+        // }
+       /*
         let todoslosdiv = document.querySelectorAll("div");
         console.log(todoslosdiv.length);
-
+*/
 
 
 
